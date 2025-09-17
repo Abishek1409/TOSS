c8c08d69dcbdc88077a56984a34a70a41cc45734
Abishek_1409 <abisheklive09@gmail.com>
2025-09-17 20:44:01 +0530

    chore(contracts): switch to ESM JS config and compile

---
+++ packages/contracts/hardhat.config.js
@@ -0,0 +1,10 @@
+﻿import '@nomicfoundation/hardhat-toolbox';
+
+const config = {
+solidity: {
+version: '0.8.24',
+settings: { optimizer: { enabled: true, runs: 200 } }
+}
+};
+
+export default config;
